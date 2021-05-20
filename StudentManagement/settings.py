a3d6ecc14aef0d7b9e47d4d920b58324e57e6be4
ABDA48 <42075569+ABDA48@users.noreply.github.com>
2021-05-20 14:34:00 +0530

    Update settings.py

--- StudentManagement/settings.py
+++ StudentManagement/settings.py
@@ -120,11 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'main/static'),
+    os.path.join(BASE_DIR,'static'),
 ]
 
 
