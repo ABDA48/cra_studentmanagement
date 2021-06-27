804105cac9aea9ee66b84562f647ed98e5382df8
ABDA48 <42075569+ABDA48@users.noreply.github.com>
2021-06-27 20:47:26 +0530

    Update views.py

--- main/views.py
+++ main/views.py
@@ -764,8 +764,10 @@ def universitybages(response):
         print(type(item))
         id=item.id
         img=qrcode.make(Root+"/university/"+str(id)+"/resume")
-        img.save("main/static/images/"+str(id)+".png")
-        cloudinary.uploader.upload("main/static/images/"+str(id)+".png")
+        img.save(str(id)+".png")
+        cloudinary.uploader.upload(str(id)+".png")
+        resultat= cloudinary.uploader.upload(str(id)+".png",overwrite =True,use_filename=True)
+        url=resultat.get("url")
     return render(response,"main/bages.html",{'ls':ls,'root':Root})
 
 def  jeunedetailbages(response):
