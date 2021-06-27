61a653c8639649450b163ae8f2062322bb5f0841
ABDA48 <42075569+ABDA48@users.noreply.github.com>
2021-06-27 16:46:09 +0530

    Update settings.py

--- StudentManagement/settings.py
+++ StudentManagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import cloudinary_storage
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'django.contrib.staticfiles',
     'main'
 ]
 
@@ -122,9 +124,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'main/static'),
 ]
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dvhzgcmie',
+    'API_KEY': '793146827816192',
+    'API_SECRET': 'ahQ_-lLuXY-8UM0c-Bemt7n_e8g'
+}
 
 
